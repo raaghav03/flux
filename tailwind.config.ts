af392cba1f42b0f14920cd1b374c446b3775c78e
raaghav03 <raghavnagpal2003@gmail.com>
2024-06-09 01:35:25 +0530

    custom font addded

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "cabinet-grotesk": ["Cabinet Grotesk", "sans-serif"],
+        geist: ["Geist", "sans-serif"],
+        "ibm-mono": ['"IBM Plex Mono"', "monospace"],
+      },
+    },
   },
   plugins: [],
 };
