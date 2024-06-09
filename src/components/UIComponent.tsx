0e105605a306b58eee814c126b90028e59d77a2e
raaghav03 <raghavnagpal2003@gmail.com>
2024-06-09 21:39:30 +0530

    bento desktop

--- src/components/UIComponent.tsx
+++ src/components/UIComponent.tsx
@@ -9,12 +9,14 @@ interface Component {
 const UIComponent: React.FC<Component> = ({ img, heading, component, variants }) => {
     return (
         <>
-            <div className='border-[1px] border-neutral-200 rounded-md w-full py-4 flex flex-col items-center justify-center overflow-hidden' >
-                <div className="w-fit">{img}</div>
-            </div>
-            <div className='flex flex-col justify-start items-start w-full font-geist'>
-                <div className='font-geist text-brand_black'>{heading}</div>
-                {(component && variants) && <div className='text-xs font-geist text-gray'> {component} components + {variants} variants</div>}
+            <div className='flex flex-col gap-4'>
+                <div className='border-[1px] border-neutral-200 rounded-md w-full py-4 flex flex-col items-center justify-center overflow-hidden lg:px-12' >
+                    <div className="w-fit">{img}</div>
+                </div>
+                <div className='flex flex-col justify-start items-start w-full font-geist'>
+                    <div className='font-geist text-brand_black'>{heading}</div>
+                    {(component && variants) && <div className='text-xs font-geist text-gray'> {component} components + {variants} variants</div>}
+                </div>
             </div></>
     )
 }
--- src/components/UiComponentGrid.tsx
+++ src/components/UiComponentGrid.tsx
@@ -8,42 +8,44 @@ import Comp5 from "../../public/assets/comp5.svg";
 import UIComponent from "./UIComponent";
 const UiComponentGrid = () => {
     return (
-        <div className="px-4 flex flex-col gap-4">
-            <div className="font-cabinet-grotesk font-bold text-lg text-brand_black">
+        <div className="px-4 flex flex-col gap-4 lg:px-12">
+            <div className="font-cabinet-grotesk lg:text-4xl font-bold text-lg text-brand_black">
                 {" "}
                 <span className="inline text-brand">1000+ components </span>for your
                 every need
             </div>
-            <UIComponent
-                img={<Comp1 />}
-                component={12}
-                variants={8}
-                heading="Button"
-            />
-            <UIComponent
-                img={<Comp2 />}
-                component={24}
-                variants={4}
-                heading="Card"
-            />
-            <UIComponent
-                img={<Comp3 />}
-                component={8}
-                variants={12}
-                heading="Chart"
-            />
-            <UIComponent
-                img={<Comp4 />}
-                component={2}
-                variants={12}
-                heading="Accordion"
-            />
-            <UIComponent
-                img={<Comp5 />}
-                component={12}
-                variants={8}
-                heading="Badge"
-            />
+            <div className="lg:grid lg:grid-cols-5 lg:gap-4">
+                <UIComponent
+                    img={<Comp1 />}
+                    component={12}
+                    variants={8}
+                    heading="Button"
+                />
+                <UIComponent
+                    img={<Comp2 />}
+                    component={24}
+                    variants={4}
+                    heading="Card"
+                />
+                <UIComponent
+                    img={<Comp3 />}
+                    component={8}
+                    variants={12}
+                    heading="Chart"
+                />
+                <UIComponent
+                    img={<Comp4 />}
+                    component={2}
+                    variants={12}
+                    heading="Accordion"
+                />
+                <UIComponent
+                    img={<Comp5 />}
+                    component={12}
+                    variants={8}
+                    heading="Badge"
+                />
+            </div>
         </div>
     );
 };
